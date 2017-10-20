fb2d9c9fddfa63a2ee2343a3c03a570df2e55bf7
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2017-10-20 23:02:55 +0900

    Update

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+@font-face {
+  font-family: "Original Yu Gothic";
+  src: local("Yu Gothic");
+  font-weight: 300;
+}
+
+@font-face {
+  font-family: "Original Yu Gothic";
+  src: local("Yu Gothic");
+  font-weight: 500;
+}
+
+@font-face {
+  font-family: "Original Yu Gothic";
+  src: local("Yu Gothic");
+  font-weight: bold;
+}
+@font-face {
+  font-family: "Helvetica Neue";
+  src: local("Helvetica Neue Regular");
+  font-weight: 100;
+}
+@font-face {
+  font-family: "Helvetica Neue";
+  src: local("Helvetica Neue Regular");
+  font-weight: 200;
+}
+
+html {
+  font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue",
+    "Original Yu Gothic", "Yu Gothic", YuGothic, Verdana, Meiryo, "M+ 1p",
+    sans-serif;
+}
+
+/* IE10以上 */
+@media all and (-ms-high-contrast: none) {
+  html {
+    font-family: Verdana, Meiryo, sans-serif;
+  }
+}
+@media all and (-ms-high-contrast: active) {
+  html {
+    font-family: Verdana, Meiryo, sans-serif;
+  }
+}
+
+body {
+  color: #333;
+  background-color: #f9f9f9;
+  margin-top: 24px;
+  margin-bottom: 24px;
+}
+</style>
---
+++ src/components/Root.vue
@@ -0,0 +1,127 @@
+<template>
+  <b-container>
+    <section>
+      <h2>仮想通貨・暗号通貨フォーセットリスト</h2>
+      <p>
+        仮想通貨・暗号通貨のフォーセット(蛇口)の一覧です。
+        <br> 絞り込み機能を使うことで、 Monacoin (MONA) 限定にしたり、 Direct Payment のみ表示することも出来ます。
+      </p>
+    </section>
+    <section>
+      <b-card title="絞り込み">
+        <b-form>
+          <b-form-row>
+            <b-col col md="auto">
+              <b-form-group label="通貨" label-for="currency">
+                <b-form-select v-model="selectedCurrency" id="currency">
+                  <option value="*">全て</option>
+                  <option v-for="currency in currencies" :key="currency.symbol" :value="currency.symbol">
+                    {{currency.name}} ({{currency.symbol}})
+                  </option>
+                </b-form-select>
+              </b-form-group>
+            </b-col>
+          </b-form-row>
+        </b-form>
+      </b-card>
+    </section>
+    <section>
+      <b-table bordered striped responsible :items="filtered()" :fields="fields">
+        <template slot="website" slot-scope="data">
+          <a :href="data.item.url" target="_blank">{{data.item.name}}</a>
+        </template>
+        <template slot="frequency" slot-scope="data">
+          {{readable(data.item.frequency)}}
+        </template>
+        <template slot="payment" slot-scope="data">
+          <template v-if="resolve('payments', 'id', data.item.payment).url === ''">
+            {{resolve('payments', 'id', data.item.payment).name}}
+          </template>
+          <template v-else>
+            <a :href="resolve('payments', 'id', data.item.payment).url" target="_blank">
+              {{resolve('payments', 'id', data.item.payment).name}}
+            </a>
+          </template>
+        </template>
+        <template slot="minimumAmount" slot-scope="data">
+          {{minimumAmount(data.item)}}
+        </template>
+      </b-table>
+    </section>
+  </b-container>
+</template>
+
+<script>
+/* eslint-disable space-before-function-paren */
+import _ from 'lodash'
+
+export default {
+  name: 'Root',
+  data () {
+    const currencies = _.sortBy(require('../data/currencies.json'), 'name')
+    const faucets = []
+    _.forEach(currencies, (w) => {
+      const _faucets = require(`../data/faucets/${w.name.toLowerCase()}.json`)
+      _.forEach(_faucets, (v) => {
+        faucets.push(Object.assign(v, {currency: w.symbol}))
+      })
+    })
+    return {
+      currencies,
+      faucets,
+      fields: {
+        currency: {label: '通貨'},
+        website: {label: 'ウェブサイト'},
+        frequency: {label: '支払い間隔'},
+        payment: {label: '支払い方法'},
+        minimumAmount: {label: '最小出金額'}
+      },
+      payments: require('../data/payments.json'),
+      selectedCurrency: '*'
+    }
+  },
+  methods: {
+    amount: function(data) {
+      return ''
+    },
+    filtered: function() {
+      if (this.$data.selectedCurrency === '*') {
+        return this.$data.faucets
+      }
+      return _.filter(this.$data.faucets, (w) => {
+        return w.currency === this.$data.selectedCurrency
+      })
+    },
+    minimumAmount: function(data) {
+      if (data.payment === 'Direct' || data.payment === 'Pooling') {
+        return data.min || 0
+      }
+      const payment = this.resolve('payments', 'id', data.payment)
+      return payment.min
+    },
+    readable: function(frequency) {
+      if (frequency >= 60) {
+        const hours = frequency / 60
+        if (hours > 1) {
+          return `${hours} hours`
+        } else {
+          return `${hours} hour`
+        }
+      } else if (frequency > 0) {
+        return `${frequency} min`
+      } else {
+        return 'Generate'
+      }
+    },
+    resolve: function(target, property, value) {
+      return this.$data[target].find((w) => w[property] === value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+section {
+  padding: 10px 0 20px 0;
+}
+</style>
